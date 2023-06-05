797c4c7a8393fa57d5545f00148c0fd44c53220b
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-06-05 16:10:57 +0600

    useCart used

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,30 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/router.jsx'
 
+// tanstack query 
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 //helmet
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Providers/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <AuthProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router}></RouterProvider>
-        </div>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router}></RouterProvider>
+          </div>
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 )
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
 import { FaShoppingCart } from 'react-icons/fa';
+import useCart from "../../../hooks/useCart";
 
 const Navbar = () => {
 
     const { user, logOut } = useContext(AuthContext);
+    const [cart] = useCart();
 
     const handleLogout = () => {
         logOut()
@@ -21,7 +23,7 @@ const Navbar = () => {
             <Link>
                 <button className="btn">
                     <FaShoppingCart/>
-                    <div className="badge badge-secondary">+99</div>
+                    <div className="badge badge-secondary">+{cart?.length || 0}</div>
                 </button>
             </Link>
         </li>
